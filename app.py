bb2d640514d084e710e98d0e927726572e83bd78
T-nahsin <nishant16405@gmail.com>
2025-09-14 23:31:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,6 @@ import pickle
 import matplotlib.pyplot as plt
 
 
-page_bg = """
-<style>
-[data-testid="stAppViewContainer"] {
-    background-color: #E6E6FA;  /* Light Violet (Lavender) */
-    color: black;
-}
-[data-testid="stSidebar"] {
-    background-color: #D8BFD8;  /* Thistle shade */
-}
-</style>
-"""
-
-st.markdown(page_bg, unsafe_allow_html=True)
-
 
 st.title('Hybrid ML Project by Nishant')
 st.header('Select Dataset to predict value!!')
